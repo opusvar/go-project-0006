1825ea854aece962bde0c58aec10e3d40cb58f61
opusvar <sprojdev@gmail.com>
2021-08-21 16:00:35 -0400

    Refactored to allow for the index printing

--- echo2.go
+++ echo2.go
@@ -7,10 +7,13 @@ import (
 
 func main() {
 	s, sep := "", ""
+	i := 0
 	for _, arg := range os.Args[1:] {
+		i++
         s += sep + arg
 		sep = " "
+		fmt.Println(s, "index", i)
 	}
+
     fmt.Println(os.Args[0])
-	fmt.Println(s)
 }
