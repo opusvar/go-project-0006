0f6aafed27aa10374dea9ba36faee37724dac511
opusvar <sprojdev@gmail.com>
2021-08-25 08:48:15 -0400

    Added response codes for status updates

--- f-url-1.5/fetch.go
+++ f-url-1.5/fetch.go
@@ -14,20 +14,19 @@ func main() {
 	for _, url := range os.Args[1:] {
 		if strings.HasPrefix(url, "http://") != true {
 			url = "http://" + url
-			fmt.Println("If 1 complete") // test code
 		}
 		resp, err := http.Get(url)
+		status := resp.Status
+		fmt.Println("Response", status)
 		if err != nil {
 			fmt.Fprint(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
-			fmt.Println("If 2 complete") // test code
 		}
 		b, err := ioutil.ReadAll(resp.Body)
 		resp.Body.Close()
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
-			fmt.Println("If 3 complete")
 		}
 		fmt.Printf("%s", b)
 	}
