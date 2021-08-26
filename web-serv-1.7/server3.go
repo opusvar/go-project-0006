ad370151cfda8bfd5bdb6093b526cbb1f2d3a86d
opusvar <sprojdev@gmail.com>
2021-08-25 21:22:48 -0400

    Webserver tests

---
+++ web-serv-1.7/server3.go
@@ -0,0 +1,24 @@
+// handler echos the HTTP request
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf("w, %s %s %s\n", r.Method, r.URL, r.Proto)
+	for k, v := range r.Header{
+		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
+	fmt.Fprintf(w, "Host = %q\n", r.Host)
+	fmt.Fprintf(w, "RemoteAdd = %q\n", r.RemoteAddr)
+	if err:= r.ParseForm(); != nil{
+        log.Print(err)
+	}
+	for k, v := range r.Form {
+		fmt.Fprint(w, "Form[%q] = %q\n", k, v)
+	}
+}
